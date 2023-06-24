d712868a93e21db730aa705699ae6e56954d58e4
FJMaster2 <80069500+FJMaster2@users.noreply.github.com>
2023-06-24 13:10:54 +0100

    Add files via upload

---
+++ frothys coffee shop 2/main.js
@@ -0,0 +1,6 @@
+ function img_one(){
+window.open('https://static.wixstatic.com/media/275112_d808c437984643b5af33380dc7936410~mv2.jpg/v1/fill/w_240,h_333,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Frothys%20menu-%20April%202023%20A4P2.jpg' , "_blank")
+}
+ function img_two(){ window.open('https://static.wixstatic.com/media/275112_fe3db8ed47b04105bd4fbe039d4ec212~mv2.jpg/v1/fill/w_235,h_333,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Frothys%20menu-%20April%202023%20A4P.jpg', "_blank")
+
+}
